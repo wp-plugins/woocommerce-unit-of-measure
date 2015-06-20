d34873909197c911470af107056b3f23c4d2a7e4
Brad Davis <Brad Davis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-20 09:58:02 +0000

    Adding tags
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-unit-of-measure/tags/1.0.1@1184674 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,49 @@
+=== WooCommerce Unit Of Measure ===
+Contributors: Brad Davis
+Tags: woocommerce, woocommerce-price
+Requires at least: 4.0
+Tested up to: 4.2.2
+Stable tag: 1.0
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+WooCommerce Unit Of Measure allows you to add a unit of measure, or any text after the price in WooCommerce.
+
+== Description ==
+WooCommerce Unit Of Measure allows you to add a unit of measure (UOM), or any text you require after the price in WooCommerce.
+
+** Requires WooCommerce to be installed. **
+
+== Installation ==
+
+= WooCommerce Compatibility =
+
+Tested on 2.3.10
+
+1. Upload WooCommerce Image Hover to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. That's it.
+
+== Frequently Asked Questions ==
+
+= Will this work with my theme? =
+Hard to say really, so many themes to test so little time.
+
+= Can I upload the unit of measure text when with WooCommerce CSV Import Suite? =
+Yes you can, follow these steps:
+- Add a column to your Import Product CSV document
+- Add the following title to your new column, meta:_woo_uom_input
+- Fill your column with your required unit of measure or whatever text you want to add after the price for your product
+
+= Can I add a unit of measure to a variation, downloadable or virtual product? =
+Accept for a product variation, yes you can.
+
+== Changelog ==
+
+= 1.0.1 =
+* Removed error on line 96, passed a variable that was not needed to the woo_uom_render_output function
+* Removed the conditional statement from the constructor
+* Renamed the return variable in the woo_uom_render_output function
+
+= 1.0 =
+* Original commit and released to the world
---
+++ woocommerce-unit-of-measure.php
@@ -0,0 +1,105 @@
+<?php
+/*
+Plugin Name: WooCommerce Unit Of Measure
+Plugin URI: 
+Description: WooCommerce Unit Of Measure allows the user to add a unit of measure after the price on WooCommerce products
+Version: 1.0
+Author: Bradley Davis
+Author URI: http://bradley-davis.com
+License: GPL3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+Text Domain: woocommerce-uom
+
+@author		 Bradley Davis
+@category    Admin
+@package	 WooCommerce RRP
+@since		 1.0.1
+
+WooCommerce Unit Of Measure. A Plugin that works with the WooCommerce plugin for WordPress.
+Copyright (C) 2014 Bradley Davis - bd@bradley-davis.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/gpl-3.0.html.
+*/
+if ( ! defined( 'ABSPATH' ) ) :
+	exit; // Exit if accessed directly
+endif;
+
+/**
+ * Check if WooCommerce is active
+ * @since 1.0
+ */
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) :
+	class Woo_UOM {
+		/**
+		 * The Constructor!
+		 * @since 1.0.1
+		 */
+		public function __construct() {
+			add_action( 'woocommerce_product_options_general_product_data', array( &$this, 'woo_uom_product_fields' ) );
+			add_action( 'woocommerce_process_product_meta', array( &$this, 'woo_uom_save_field_input' ) );
+			// Render the uom field output on the frontend
+			add_filter( 'woocommerce_get_price_html', array( &$this, 'woo_uom_render_output' ) );
+		}
+
+		/**
+		 * Add the custom fields or the UOM to the prodcut general tab
+		 * @since 1.0
+		 */
+		public function woo_uom_product_fields() {
+		  global $woocommerce, $post;
+		  
+			echo '<div class="wc_uom_input">'; 
+				// Woo_UOM fields will be created here. 
+				woocommerce_wp_text_input( 
+					array( 
+						'id'          => '_woo_uom_input', 
+						'label'       => __( 'Unit of Measure', 'woo_uom' ), 
+						'placeholder' => '',
+						'desc_tip'    => 'true',
+						'description' => __( 'Enter your unit of measure for this product here.', 'woo_uom' ) 
+					)
+				);
+			echo '</div>';
+		}
+
+		/**
+		 * Update the database with the new input
+		 * @since 1.0
+		 */
+		public function woo_uom_save_field_input( $post_id ){
+			// Woo_UOM text field
+			$woo_uom_input = $_POST['_woo_uom_input'];
+			if ( ! empty( $woo_uom_input ) ) :
+				update_post_meta( $post_id, '_woo_uom_input', esc_attr( $woo_uom_input ) );
+			endif;
+		}
+
+		/**
+		 * Render the output
+		 * @since 1.0.1
+		 * @return $price + UOM string
+		 */
+		public function woo_uom_render_output( $price ) {
+			global $woocommerce, $post;
+			// Display Custom Field Value
+			$woo_uom_output = get_post_meta( $post->ID, '_woo_uom_input', true );
+
+			return $price . '&nbsp;' . $woo_uom_output;
+		}
+	} // END class Woo_UOM
+
+	// Instantiate the class
+	$woo_uom = new Woo_UOM();
+
+endif; // If WC is active
